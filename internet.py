1119181c8cd9f26e7ef7da8ce81a347adec62686
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-01 19:00:29 +0530

    Create internet.py

---
+++ internet.py
@@ -0,0 +1,10 @@
+import socket
+
+# Check for internet connection before launching the main UI
+
+def is_connected():
+    try:
+        socket.create_connection(("1.1.1.1", 53), timeout=3)
+        return True
+    except OSError:
+        return False
