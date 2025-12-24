907e6bb56aef724c85935fb34f6dec1f892b0fe7
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-12-24 09:46:58 +0530

    Delete app-portal/app/firefox directory

--- app-portal/app/firefox/about.js
+++
@@ -1,4 +0,0 @@
-// This file can be used for any interactive elements on the app's about page.
-// For this example, it is intentionally left empty.
-// For instance, you could add a script to handle a "copy install command" button.
-console.log("Firefox about page script loaded.");
