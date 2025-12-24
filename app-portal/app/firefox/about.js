bf4bbf1bdbb312b5963b83c91428fb56fdaa7ee2
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-12-24 09:47:33 +0530

    Add files via upload

---
+++ app-portal/app/firefox/about.js
@@ -0,0 +1,4 @@
+// This file can be used for any interactive elements on the app's about page.
+// For this example, it is intentionally left empty.
+// For instance, you could add a script to handle a "copy install command" button.
+console.log("Firefox about page script loaded.");
